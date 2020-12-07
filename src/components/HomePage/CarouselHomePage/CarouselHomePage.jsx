324f9a33763383c3c36a636d001a582d4385540d
andreyudi10 <andrianus.yudi@gmail.com>
2020-12-07 08:48:28 +0700

    add dynamic picture array

--- src/components/HomePage/CarouselHomePage/CarouselHomePage.jsx
+++ src/components/HomePage/CarouselHomePage/CarouselHomePage.jsx
@@ -2,43 +2,35 @@ import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer }
 import styles from './CarouselHomePage.module.css'
 
 const CarouselHomePage = () =>{
+
+    const listPict = ["https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg",
+                      "https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg",
+                      "https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"]
+
+    const pictComponent = (
+      listPict.map((val,idx)=>(
+            <MDBCarouselItem itemId={`${idx+1}`}>
+              <MDBView>
+                <img
+                  className="d-block w-100"
+                  src={`${val}`}
+                  alt={`${idx} slide`}
+                />
+              </MDBView>
+            </MDBCarouselItem>
+      ))
+    )
     return(
         <MDBContainer className={styles.container}>
         <MDBCarousel
           activeItem={1}
-          length={3}
+          length={listPict.length}
           showControls={true}
           showIndicators={true}
           className="z-depth-1"
         >
           <MDBCarouselInner>
-            <MDBCarouselItem itemId="1">
-              <MDBView>
-                <img
-                  className="d-block w-100"
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
-                  alt="First slide"
-                />
-              </MDBView>
-            </MDBCarouselItem>
-            <MDBCarouselItem itemId="2">
-              <MDBView>
-                <img
-                  className="d-block w-100"
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
-                  alt="Second slide"
-                />
-              </MDBView>
-            </MDBCarouselItem>
-            <MDBCarouselItem itemId="3">
-              <MDBView>
-                <img
-                  className="d-block w-100"
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
-                  alt="Third slide"
-                />
-              </MDBView>
-            </MDBCarouselItem>
+              {pictComponent}            
           </MDBCarouselInner>
         </MDBCarousel>
       </MDBContainer>
